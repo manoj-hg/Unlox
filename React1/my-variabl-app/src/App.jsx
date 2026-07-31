484dcf4308f938a5ff6239d862ac0c834be3d689
manoj-hg <manojhg321@gmail.com>
2026-07-31 22:04:59 +0530

    Hooks

--- React1/my-variabl-app/src/App.jsx
+++ React1/my-variabl-app/src/App.jsx
@@ -1,16 +1,92 @@
-import React from "react";
+//Example 1
+
+
+// import React from "react";
+
+// function App(){
+
+// let username = "Dinesh"
+
+// function Greet(){
+//    alert(`Good Evening ${username}`)
+// }
+
+// return(
+//   <>
+//   <h2>My Name is : {username}  </h2>
+
+//   <button onClick={Greet}>Greet</button>
+//   </>
+// )
+
+// }
+// export default App
+
+
+
+//Example 2 - Variable vs React Variable
+// import React from "react";
+
+// function App(){
+
+// let count = 10
+
+// function IncreaseCount(){
+//   count = count + 1;
+//   console.log(count)
+// }
+
+// return(
+//   <>
+//   <h2>Like/Cart/Quanity :  {count} </h2>
+
+//   <button onClick={IncreaseCount}>Increase</button>
+//   </>
+// )
+
+// }
+// export default App
+
+
+//Exampl3 useState Varibale
+
+// import React , {useState} from "react";
+
+// function App(){
+
+// const [like, setLike] = useState(12)
+
+// function IncreaseLike(){
+//   setLike(like + 1)
+//   console.log(like)
+// }
+
+// return(
+//   <>
+//   <h2>Like/Cart/Quanity :  {like} </h2>
+  
+//   <button onClick={IncreaseLike}>Increase</button>
+//   </>
+// )
+
+// }
+// export default App
+
+
+import React , {useState} from "react";
 
 function App(){
-      let name ="Manoj"
-      function Greet(){
 
-      }
-  return(
-    <>
-    <h2>Hello My name is : {name}</h2>
-    </>
-  )
+const[show,setShow] = useState(false)
+
+
+
+return(
+  <>
+   <input type={show ? "text" : "password"} placeholder="Enter Your Password"        />
+   <button onClick={ ()=>   setShow(!show)}>Show/Hide</button>
+  </>
+)
+
 }
-
-
-export default App;
+export default App
