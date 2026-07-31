18813e404da7129aaa5d535ee742ad713cd72dff
manoj-hg <manojhg321@gmail.com>
2026-07-31 20:57:43 +0530

    My-variable-react

---
+++ React1/my-variabl-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App(){
+      let name ="Manoj"
+      function Greet(){
+
+      }
+  return(
+    <>
+    <h2>Hello My name is : {name}</h2>
+    </>
+  )
+}
+
+
+export default App;
