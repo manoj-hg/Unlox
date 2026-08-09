c7fad3e259a0fd3bf0ea0b26ef7a0c909c7b5ef9
manoj-hg <manojhg321@gmail.com>
2026-08-09 13:43:24 +0530

    React-Bootstrap

---
+++ React2/my-bootstrap-r/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cartbtn from "./Btn";
+
+function App() {
+  return (
+    <>
+      <h1>React Bootstrap</h1>
+      <Cartbtn />
+    </>
+  );
+}
+
+export default App;
